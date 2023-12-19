731ec8e4eea1d85e358bb93fb3314cb4b982b2ea
x <x@x>
2023-12-19 14:17:02 +0000

    tor test

---
+++ tor-test.py
@@ -0,0 +1,9 @@
+# install pysocks
+import socks
+import socket
+socks.set_default_proxy(socks.SOCKS5, "127.0.0.1", 9050)
+socket.socket = socks.socksocket
+import requests
+response = requests.get("https://icanhazip.com")
+print(response.content)
+
